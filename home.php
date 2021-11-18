1d2f9f49b94b14bfb40ad8630a9818c3f4371f46
nurmambetovaiu <81546031+nurmambetovaiu@users.noreply.github.com>
2021-11-18 14:56:57 +0600

    Update home.php

--- home.php
+++ home.php
@@ -2,11 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="second.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>portfolio</title>
 </head>
 <body>
-    <h1>Welcome, successfully login </h1>
+    
+    <h1>Nurmambetov Kemel</h1> <img src="Hasbulla-Wallpapers-9.jpg" alt="" style="width: 300px; height: 300px;" >
+    <style>
+        body {
+            background-image: url(https://wallpaperaccess.com/full/4487462.jpg);
+            background-size: cover; 
+        }
+        form {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background: rgba(0,0,0,.7);
+}
+    </style>
+    <h2>+996704 330 204</h2>
+    
 </body>
 </html>
